c95a185ea9b2b4714d2b5a52370c4ff486594e2e
Gavin Lowry <gavin.lowry@endurance.com>
2019-09-27 09:03:07 -0600

    backend folder created with barebones server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config({path: '.env'});
+
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB connected');
+});
+
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
